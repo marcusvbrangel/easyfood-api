c0887601317c3761bda47889b7b90e397f5c940b
Marcus Rangel <marcus.vbrangel@gmail.com>
2024-05-02 19:18:17 -0300

    feat: add application.properties file

--- src/main/java/com/marvin/easyfoodapi/EasyFoodApiApplication.java
+++ src/main/java/com/marvin/easyfoodapi/EasyFoodApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlgafoodApiApplication {
+public class EasyFoodApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AlgafoodApiApplication.class, args);
+		SpringApplication.run(EasyFoodApiApplication.class, args);
 
 	}
 
