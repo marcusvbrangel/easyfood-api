c5727bd811ae81288006cd32ea799d7e164e7cc1
Marcus Rangel <marcus.vbrangel@gmail.com>
2024-05-05 22:02:02 -0300

    feat: add sql include restaurante

--- src/main/java/com/marvin/easyfoodapi/domain/model/Restaurante.java
+++ src/main/java/com/marvin/easyfoodapi/domain/model/Restaurante.java
@@ -1,12 +1,21 @@
 package com.marvin.easyfoodapi.domain.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.Objects;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 @Entity
 public class Restaurante {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,40 +24,4 @@ public class Restaurante {
 
     private BigDecimal taxaFrete;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public BigDecimal getTaxaFrete() {
-        return taxaFrete;
-    }
-
-    public void setTaxaFrete(BigDecimal taxaFrete) {
-        this.taxaFrete = taxaFrete;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Restaurante that = (Restaurante) o;
-        return Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,7 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Mexicana');
 insert into cozinha (nome) values ('Brasileira');
+
+insert into restaurante(nome, taxa_frete) values ('Casa Velha', 10.99);
+insert into restaurante(nome, taxa_frete) values ('Costa Marinha', 9.05);
+insert into restaurante(nome, taxa_frete) values ('Boteco do Zé', 4.00);
