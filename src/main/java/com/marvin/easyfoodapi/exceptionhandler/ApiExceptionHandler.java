327896dfd8e1892a21df57d101929341e5782455
Marcus Rangel <marcus.vbrangel@gmail.com>
2024-05-26 17:45:47 -0300

    feat: add a handler execption for media type not supported

--- src/main/java/com/marvin/easyfoodapi/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/marvin/easyfoodapi/exceptionhandler/ApiExceptionHandler.java
@@ -4,22 +4,25 @@ import com.marvin.easyfoodapi.domain.exception.EntidadeEmUsoException;
 import com.marvin.easyfoodapi.domain.exception.EntidadeExistenteException;
 import com.marvin.easyfoodapi.domain.exception.EntidadeNaoEcontradaException;
 import com.marvin.easyfoodapi.domain.exception.NegocioException;
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.lang.Nullable;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.List;
 
 // RFC 7807
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    public static final String HTTP_MESSAGE_NOT_READABLE_DETAIL =
+        "O corpo da requisição está inválido. Verifique erro de sintaxe.";
+    public static final String HTTP_MEDIA_TYPE_NOT_SUPPORTED_DETAIL =
+        "Somente formato de requisição (JSON) é suportado. Verifique o cabeçalho da requisição.";
 
     @ExceptionHandler(EntidadeNaoEcontradaException.class)
     public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEcontradaException ex, WebRequest request) {
@@ -88,8 +91,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleHttpMessageNotReadable(
         HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         ProblemType problemType = ProblemType.HTTP_MESSAGE_NOT_READABLE;
-        String detail = "O corpo da requisição está inválido. Verifique erro de sintaxe.";
-        Problem problem = this.createProblemBuilder(status, problemType, detail).build();
+        Problem problem = this.createProblemBuilder(status, problemType, HTTP_MESSAGE_NOT_READABLE_DETAIL).build();
         return this.handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
 
@@ -97,8 +99,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
         HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         ProblemType problemType = ProblemType.HTTP_MEDIA_TYPE_NOT_SUPPORTED;
-        String detail = "Somente o formato de requisição (JSON) é suportado. Verifique o cabeçalho da requisição.";
-        Problem problem = this.createProblemBuilder(status, problemType, detail).build();
+        Problem problem = this.createProblemBuilder(status, problemType, HTTP_MEDIA_TYPE_NOT_SUPPORTED_DETAIL).build();
         return this.handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
 
